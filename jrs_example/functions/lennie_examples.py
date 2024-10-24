48ff20259b840b86bd64850e1a9cdef1419adfb7
W-L-W <lennie499@gmail.com>
2024-10-24 17:14:19 +0100

    renamed from src and added examples

---
+++ jrs_example/functions/lennie_examples.py
@@ -0,0 +1,33 @@
+import logging
+import random
+
+log = logging.getLogger(__name__)
+
+
+def test_multi_arg_config(
+    root_arg: str,
+    arg1: int,
+    arg2: bool,
+    patch_arg: str,
+    expt_input_format: str,
+    default_arg: str = "default argument",
+    rng_seed: int = 42,
+    printq: bool = False,
+) -> None:
+
+    print("Testing simple config")
+
+    random.seed(rng_seed)
+    runif = random.random()
+
+    arg_description_string =f""""
+        root_arg: {root_arg}
+        arg1: {arg1}
+        arg2: {arg2}
+        patch_arg: {patch_arg}
+        expt_input_format: {expt_input_format}
+        default_arg: {default_arg}
+        rng: {runif}
+        """
+    if printq:
+        print(arg_description_string)
