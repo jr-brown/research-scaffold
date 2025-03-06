46751de5e7415a68b99af4fdd2430024d7e254fc
Jason Brown <jasonrbrown@virginmedia.com>
2025-03-06 11:50:44 +0000

    Argparsing

--- research_scaffold/__init__.py
+++ research_scaffold/__init__.py
@@ -8,5 +8,6 @@ from beartype.claw import beartype_this_package
 beartype_this_package()
 
 from .config_tools import execute_experiments
+from .argparsing import get_base_argparser, process_base_args
 from .util import get_logger
 
---
+++ research_scaffold/argparsing.py
@@ -0,0 +1,54 @@
+import logging
+import argparse
+
+from os import environ
+
+
+def get_base_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Run experiments/analyses/tests from configs"
+    )
+    parser.add_argument("-c", "--config_path", default=None, help="Config file to run")
+    parser.add_argument(
+        "-m",
+        "--meta_config_path",
+        default=None,
+        help="Meta config file that can specify many experiments, each possibly composing configs",
+    )
+    parser.add_argument(
+        "-l", "--loglevel", default="info", help="Provide logging level"
+    )
+    parser.add_argument(
+        "-e",
+        "--externalLoglevel",
+        default="warning",
+        help="Provide logging level for external libraries",
+    )
+    parser.add_argument(
+        "-f",
+        "--force_cpu",
+        default=False,
+        action="store_true",
+        help="Force CPU usage instead of GPU/TPU",
+    )
+    return parser
+
+
+def process_base_args(args) -> tuple[str, str]:
+    logging.basicConfig(
+        level=args.loglevel.upper(),
+        format="%(asctime)s %(module)-15s %(levelname)-8s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    # TODO: Extend this to all external libraries
+    external_logger_names = ["absl", "jax"]
+
+    for name in external_logger_names:
+        logging.getLogger(name).setLevel(args.externalLoglevel.upper())
+
+    if args.force_cpu:
+        environ["CUDA_VISIBLE_DEVICES"] = ""
+
+    return args.config_path, args.meta_config_path
+
