5fd4a5644a180d2e69aa531c44fac0c776b92d97
Lennie Wells <lennie499@gmail.com>
2025-04-28 18:40:52 +0100

    implemented bonus function kwargs!

--- example/functions/lennie_examples.py
+++ example/functions/lennie_examples.py
@@ -1,5 +1,6 @@
 import logging
 import random
+from typing import Optional
 
 log = logging.getLogger(__name__)
 
@@ -10,6 +11,7 @@ def test_multi_arg_config(
     arg2: bool,
     patch_arg: str,
     expt_input_format: str,
+    bonus_arg: Optional[str] = None,
     default_arg: str = "default argument",
     rng_seed: int = 42,
     printq: bool = False,
@@ -20,12 +22,13 @@ def test_multi_arg_config(
     random.seed(rng_seed)
     runif = random.random()
 
-    arg_description_string =f""""
+    arg_description_string = f""""
         root_arg: {root_arg}
         arg1: {arg1}
         arg2: {arg2}
         patch_arg: {patch_arg}
         expt_input_format: {expt_input_format}
+        bonus_arg: {bonus_arg}
         default_arg: {default_arg}
         rng: {runif}
         """
